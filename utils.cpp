3e651fc1d6997eecbb15630f015b8ced609455eb
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2023-04-12 07:53:16 +0200

    Added create buffer function

--- utils.cpp
+++ utils.cpp
@@ -14,7 +14,7 @@ struct Buffer {
 extern "C" {
 #endif
     char* finish_string(char* start);
-
+    Buffer* create_buffer(int width, int height);
 #ifdef __cplusplus
 }
 
@@ -25,3 +25,15 @@ char* finish_string(char* start){
     return (char*)str.c_str();
 }
 
+
+Buffer* create_buffer(int width, int height){
+    Buffer* buffer = new Buffer();
+    buffer->data = new Vec3[width * height];
+    buffer->size = width * height;
+    for (int i = 0; i < buffer->size; i++){
+        buffer->data[i].x = 1.0f;
+        buffer->data[i].y = 1.0f;
+        buffer->data[i].z = 1.0f;
+    }
+    return buffer;
+}
