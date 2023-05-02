6b4f4c7c1d4b77b311809402062f715017b0efb4
Szymon <szymon.ozog@gmail.com>
2023-05-02 10:33:37 +0200

    Fix rotation axes

--- run.py
+++ run.py
@@ -36,8 +36,8 @@ def on_move(x,y):
     global mouse_y
     if mouse_pressed:
         print(x, mouse_x)
-        view_info.rotX = (view_info.rotX + ((x-mouse_x) * 0.001 * frame_time)) % (2 * math.pi)  
-        view_info.rotY = (view_info.rotY + ((y-mouse_y) * 0.001 * frame_time)) % (2 * math.pi)
+        view_info.rotY = (view_info.rotY + ((x-mouse_x) * 0.001 * frame_time)) % (2 * math.pi)  
+        view_info.rotX = (view_info.rotX + ((y-mouse_y) * 0.001 * frame_time)) % (2 * math.pi)
         mouse_y=y
         mouse_x=x 
 
