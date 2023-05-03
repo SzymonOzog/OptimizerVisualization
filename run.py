a74d86859490edb7539c24614de6a9048e9e7940
Szymon <szymon.ozog@gmail.com>
2023-05-03 14:08:28 +0200

    Bring back compilation

--- run.py
+++ run.py
@@ -11,5 +11,8 @@ from cpp_classes import *
 from engine import *
 
 if __name__ == '__main__':
+    if not os.path.exists("obj"):
+        os.mkdir("obj")
+    os.system('g++ -static -shared -o obj/libbuffer.dll source/*.cpp')
     engine = Engine(500,500)
     engine.start()
