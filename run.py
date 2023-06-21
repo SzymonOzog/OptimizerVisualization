3ab45d7b3856c012e261d93aedba30e3491cbfc7
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2023-06-21 07:02:29 +0200

    import cleanup

--- run.py
+++ run.py
@@ -1,13 +1,4 @@
-import ctypes
-import pathlib
-from ctypes.util import find_library
 import os
-import numpy as np
-import cv2
-import math
-import time
-from pynput import mouse, keyboard
-from cpp_classes import *
 from engine import *
 
 if __name__ == '__main__':
