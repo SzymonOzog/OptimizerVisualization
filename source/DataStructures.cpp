6a4c43e950600b807f8041c81abf202bbacf467b
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2023-06-29 09:21:11 +0200

    Split into header and source file

---
+++ source/DataStructures.cpp
@@ -0,0 +1,234 @@
+#include "DataStructures.h"
+#include "Math.h"
+
+float Vec3::length()
+{
+    return sqrt(x*x + y*y + z*z);
+}
+
+void Vec3::normalize()
+{
+    float len = length();
+    x/=len;
+    y/=len;
+    z/=len;
+}
+
+Vec3 Vec3::operator += (const Vec3& other)
+{
+    x += other.x;
+    y += other.y;
+    z += other.z;
+    return *this;
+}
+Vec3 Vec3::operator -= (const Vec3& other)
+{
+    x -= other.x;
+    y -= other.y;
+    z -= other.z;
+    return *this;
+}
+Vec3 Vec3::operator *= (const float& other)
+{
+    x *= other;
+    y *= other;
+    z *= other;
+    return *this;
+}
+Vec3 Vec3::operator /= (const float& other)
+{
+    x /= other;
+    y /= other;
+    z /= other;
+    return *this;
+}
+Vec3 Vec3::operator + (const Vec3& other) const
+{
+    Vec3 result = *this;
+    result += other;
+    return result;
+}
+Vec3 Vec3::operator - (const Vec3& other) const
+{
+    Vec3 result = *this;
+    result -= other;
+    return result;
+}
+Vec3 Vec3::operator * (const float& other) const
+{
+    Vec3 result = *this;
+    result *= other;
+    return result;
+}
+Vec3 Vec3::operator / (const float& other) const
+{
+    Vec3 result = *this;
+    result /= other;
+    return result;
+}
+
+
+Mat3 Mat3::Identity()
+{
+    Mat3 result;
+    result.data[0][0] = 1.0f;
+    result.data[1][1] = 1.0f;
+    result.data[2][2] = 1.0f;
+    return result;
+}
+
+Mat3 Mat3::RotationZ(float angle)
+{
+    Mat3 result;
+    result.data[0][0] = cos(angle);
+    result.data[0][1] = -sin(angle);
+    result.data[1][0] = sin(angle);
+    result.data[1][1] = cos(angle);
+    result.data[2][2] = 1.0f;
+    return result;
+}
+
+Mat3 Mat3::RotationY(float angle)
+{
+    Mat3 result;
+    result.data[0][0] = cos(angle);
+    result.data[0][2] = sin(angle);
+    result.data[1][1] = 1.0f;
+    result.data[2][0] = -sin(angle);
+    result.data[2][2] = cos(angle);
+    return result;
+}
+
+Mat3 Mat3::RotationX(float angle)
+{
+    Mat3 result;
+    result.data[0][0] = 1.0f;
+    result.data[1][1] = cos(angle);
+    result.data[1][2] = -sin(angle);
+    result.data[2][1] = sin(angle);
+    result.data[2][2] = cos(angle);
+    return result;
+}
+
+
+Vec3 Mat3::operator*(const Vec3& other)
+{
+    Vec3 result;
+    result.x = data[0][0] * other.x + data[0][1] * other.y + data[0][2] * other.z;
+    result.y = data[1][0] * other.x + data[1][1] * other.y + data[1][2] * other.z;
+    result.z = data[2][0] * other.x + data[2][1] * other.y + data[2][2] * other.z;
+    return result;
+}
+
+Mat3 Mat3::operator*(const Mat3& other)
+{
+    Mat3 result;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            result.data[i][j] = data[i][0] * other.data[0][j] + data[i][1] * other.data[1][j] + data[i][2] * other.data[2][j];
+        }
+    }
+    return result;
+}
+
+
+Mat4 Mat4::identity()
+{
+    Mat4 result;
+    result.data[0][0] = 1.0f;
+    result.data[1][1] = 1.0f;
+    result.data[2][2] = 1.0f;
+    result.data[3][3] = 1.0f;
+    return result;
+}
+
+Mat4 Mat4::projection(float FOV, float aspectRatio, float nearPlane, float farPlane)
+{
+    Mat4 result;
+    float FOVRad = 1.0f / tan(FOV * 0.5f / 180.0f * 3.14159f);
+    result.data[0][0] = aspectRatio * FOVRad;
+    result.data[1][1] = FOVRad;
+    result.data[2][2] = farPlane / (farPlane - nearPlane);
+    result.data[3][2] = (-farPlane * nearPlane) / (farPlane - nearPlane);
+    result.data[2][3] = 1.0f;
+    result.data[3][3] = 0.0f;
+    return result;
+}
+
+Mat4 Mat4::translation(const Vec3& translation)
+{
+    Mat4 result = identity();
+    result.data[3][0] = translation.x;
+    result.data[3][1] = translation.y;
+    result.data[3][2] = translation.z;
+    return result;
+}
+
+Mat4 Mat4::rotationX(float angle)
+{
+    Mat4 result = identity();
+    result.data[1][1] = cos(angle);
+    result.data[1][2] = -sin(angle);
+    result.data[2][1] = sin(angle);
+    result.data[2][2] = cos(angle);
+    return result;
+}
+
+Mat4 Mat4::rotationY(float angle)
+{
+    Mat4 result = identity();
+    result.data[0][0] = cos(angle);
+    result.data[0][2] = sin(angle);
+    result.data[2][0] = -sin(angle);
+    result.data[2][2] = cos(angle);
+    return result;
+}
+
+Mat4 Mat4::rotationZ(float angle)
+{
+    Mat4 result = identity();
+    result.data[0][0] = cos(angle);
+    result.data[0][1] = -sin(angle);
+    result.data[1][0] = sin(angle);
+    result.data[1][1] = cos(angle);
+    return result;
+}
+
+Mat4 Mat4::transpose()
+{
+    Mat4 result = identity();
+    for (int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            result.data[i][j] = data[j][i];
+        }
+    }
+    return result;
+}
+
+Vec4 Mat4::operator* (const Vec4& RHS)
+{
+    Vec4 result;
+    result.x = data[0][0] * RHS.x + data[1][0] * RHS.y + data[2][0] * RHS.z + data[3][0] * RHS.w;
+    result.y = data[0][1] * RHS.x + data[1][1] * RHS.y + data[2][1] * RHS.z + data[3][1] * RHS.w;
+    result.z = data[0][2] * RHS.x + data[1][2] * RHS.y + data[2][2] * RHS.z + data[3][2] * RHS.w;
+    result.w = data[0][3] * RHS.x + data[1][3] * RHS.y + data[2][3] * RHS.z + data[3][3] * RHS.w;
+    return result;
+}
+
+Mat4 Mat4::operator * (const Mat4& RHS)
+{
+    Mat4 result;
+    for (int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            result.data[i][j] = data[i][0] * RHS.data[0][j] + data[i][1] * RHS.data[1][j] + data[i][2] * RHS.data[2][j] + data[i][3] * RHS.data[3][j];
+        }
+
+    }
+    return result;
+}
--- source/DataStructures.h
+++ source/DataStructures.h
@@ -7,71 +7,17 @@ struct Vec3
 {
     float x, y, z;
 
-    float length()
-    {
-        return sqrt(x*x + y*y + z*z);
-    }
+    float length();
+    void normalize();
 
-    void normalize()
-    {
-        float len = length();
-        x/=len;
-        y/=len;
-        z/=len;
-    }
-
-    Vec3 operator += (const Vec3& other)
-    {
-        x += other.x;
-        y += other.y;
-        z += other.z;
-        return *this;
-    }
-    Vec3 operator -= (const Vec3& other)
-    {
-        x -= other.x;
-        y -= other.y;
-        z -= other.z;
-        return *this;
-    }
-    Vec3 operator *= (const float& other)
-    {
-        x *= other;
-        y *= other;
-        z *= other;
-        return *this;
-    }
-    Vec3 operator /= (const float& other)
-    {
-        x /= other;
-        y /= other;
-        z /= other;
-        return *this;
-    }
-    Vec3 operator + (const Vec3& other) const
-    {
-        Vec3 result = *this;
-        result += other;
-        return result;
-    }
-    Vec3 operator - (const Vec3& other) const
-    {
-        Vec3 result = *this;
-        result -= other;
-        return result;
-    }
-    Vec3 operator * (const float& other) const
-    {
-        Vec3 result = *this;
-        result *= other;
-        return result;
-    }
-    Vec3 operator / (const float& other) const
-    {
-        Vec3 result = *this;
-        result /= other;
-        return result;
-    }
+    Vec3 operator += (const Vec3& other);
+    Vec3 operator -= (const Vec3& other);
+    Vec3 operator *= (const float& other);
+    Vec3 operator /= (const float& other);
+    Vec3 operator + (const Vec3& other) const;
+    Vec3 operator - (const Vec3& other) const;
+    Vec3 operator * (const float& other) const;
+    Vec3 operator / (const float& other) const;
 
 
 };
@@ -81,9 +27,7 @@ struct Vec4 : Vec3
     float w;
 
     Vec4() : Vec3({0.0f, 0.0f, 0.0f}), w(1.0f) {}
-
     Vec4(float x, float y, float z, float w) : Vec3({x, y, z}), w(w) {}
-
     Vec4(const Vec3& other) : Vec3(other), w(1.0f) {}
 };
 
@@ -91,80 +35,15 @@ struct Mat3
 {
     std::vector<std::vector<float>> data;
 
-    Mat3()
-    {
-        data.resize(3);
-        for (int i = 0; i < 3; i++)
-        {
-            data[i].resize(3);
-            data[i] = { 0.0f, 0.0f, 0.0f };
-        }
-    }
+    Mat3() : data(3, { 0.0f, 0.0f, 0.0f }) {}
 
-    static Mat3 Identity()
-    {
-        Mat3 result;
-        result.data[0][0] = 1.0f;
-        result.data[1][1] = 1.0f;
-        result.data[2][2] = 1.0f;
-        return result;
-    }
+    static Mat3 Identity();
+    static Mat3 RotationZ(float angle);
+    static Mat3 RotationY(float angle);
+    static Mat3 RotationX(float angle);
 
-    static Mat3 RotationZ(float angle)
-    {
-        Mat3 result;
-        result.data[0][0] = cos(angle);
-        result.data[0][1] = -sin(angle);
-        result.data[1][0] = sin(angle);
-        result.data[1][1] = cos(angle);
-        result.data[2][2] = 1.0f;
-        return result;
-    }
-
-    static Mat3 RotationY(float angle)
-    {
-        Mat3 result;
-        result.data[0][0] = cos(angle);
-        result.data[0][2] = sin(angle);
-        result.data[1][1] = 1.0f;
-        result.data[2][0] = -sin(angle);
-        result.data[2][2] = cos(angle);
-        return result;
-    }
-
-    static Mat3 RotationX(float angle)
-    {
-        Mat3 result;
-        result.data[0][0] = 1.0f;
-        result.data[1][1] = cos(angle);
-        result.data[1][2] = -sin(angle);
-        result.data[2][1] = sin(angle);
-        result.data[2][2] = cos(angle);
-        return result;
-    }
-
-
-    Vec3 operator*(const Vec3& other)
-    {
-        Vec3 result;
-        result.x = data[0][0] * other.x + data[0][1] * other.y + data[0][2] * other.z;
-        result.y = data[1][0] * other.x + data[1][1] * other.y + data[1][2] * other.z;
-        result.z = data[2][0] * other.x + data[2][1] * other.y + data[2][2] * other.z;
-        return result;
-    }
-
-    Mat3 operator*(const Mat3& other)
-    {
-        Mat3 result;
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                result.data[i][j] = data[i][0] * other.data[0][j] + data[i][1] * other.data[1][j] + data[i][2] * other.data[2][j];
-            }
-        }
-        return result;
-    }
+    Vec3 operator*(const Vec3& other);
+    Mat3 operator*(const Mat3& other);
 };
 
 struct Mat4
@@ -173,104 +52,16 @@ struct Mat4
 
     Mat4() : data(4,  { 0.0f, 0.0f, 0.0f, 0.0f }) {}
 
-    static Mat4 identity()
-    {
-        Mat4 result;
-        result.data[0][0] = 1.0f;
-        result.data[1][1] = 1.0f;
-        result.data[2][2] = 1.0f;
-        result.data[3][3] = 1.0f;
-        return result;
-    }
+    static Mat4 identity();
+    static Mat4 projection(float FOV, float aspectRatio, float nearPlane, float farPlane);
+    static Mat4 translation(const Vec3& translation);
+    static Mat4 rotationX(float angle);
+    static Mat4 rotationY(float angle);
+    static Mat4 rotationZ(float angle);
 
-    static Mat4 projection(float FOV, float aspectRatio, float nearPlane, float farPlane)
-    {
-        Mat4 result;
-        float FOVRad = 1.0f / tan(FOV * 0.5f / 180.0f * 3.14159f);
-        result.data[0][0] = aspectRatio * FOVRad;
-        result.data[1][1] = FOVRad;
-        result.data[2][2] = farPlane / (farPlane - nearPlane);
-        result.data[3][2] = (-farPlane * nearPlane) / (farPlane - nearPlane);
-        result.data[2][3] = 1.0f;
-        result.data[3][3] = 0.0f;
-        return result;
-    }
-
-    static Mat4 translation(const Vec3& translation)
-    {
-        Mat4 result = identity();
-        result.data[3][0] = translation.x;
-        result.data[3][1] = translation.y;
-        result.data[3][2] = translation.z;
-        return result;
-    }
-
-    static Mat4 rotationX(float angle)
-    {
-        Mat4 result = identity();
-        result.data[1][1] = cos(angle);
-        result.data[1][2] = -sin(angle);
-        result.data[2][1] = sin(angle);
-        result.data[2][2] = cos(angle);
-        return result;
-    }
-
-    static Mat4 rotationY(float angle)
-    {
-        Mat4 result = identity();
-        result.data[0][0] = cos(angle);
-        result.data[0][2] = sin(angle);
-        result.data[2][0] = -sin(angle);
-        result.data[2][2] = cos(angle);
-        return result;
-    }
-
-    static Mat4 rotationZ(float angle)
-    {
-        Mat4 result = identity();
-        result.data[0][0] = cos(angle);
-        result.data[0][1] = -sin(angle);
-        result.data[1][0] = sin(angle);
-        result.data[1][1] = cos(angle);
-        return result;
-    }
-
-    Mat4 transpose()
-    {
-        Mat4 result = identity();
-        for (int i = 0; i < 4; i++)
-        {
-            for(int j = 0; j < 4; j++)
-            {
-                result.data[i][j] = data[j][i];
-            }
-        }
-        return result;
-    }
-
-    Vec4 operator* (const Vec4& RHS)
-    {
-        Vec4 result;
-        result.x = data[0][0] * RHS.x + data[1][0] * RHS.y + data[2][0] * RHS.z + data[3][0] * RHS.w;
-        result.y = data[0][1] * RHS.x + data[1][1] * RHS.y + data[2][1] * RHS.z + data[3][1] * RHS.w;
-        result.z = data[0][2] * RHS.x + data[1][2] * RHS.y + data[2][2] * RHS.z + data[3][2] * RHS.w;
-        result.w = data[0][3] * RHS.x + data[1][3] * RHS.y + data[2][3] * RHS.z + data[3][3] * RHS.w;
-        return result;
-    }
-
-    Mat4 operator * (const Mat4& RHS)
-    {
-        Mat4 result;
-        for (int i = 0; i < 4; i++)
-        {
-            for(int j = 0; j < 4; j++)
-            {
-                result.data[i][j] = data[i][0] * RHS.data[0][j] + data[i][1] * RHS.data[1][j] + data[i][2] * RHS.data[2][j] + data[i][3] * RHS.data[3][j];
-            }
-
-        }
-        return result;
-    }
+    Mat4 transpose();
+    Vec4 operator* (const Vec4& RHS);
+    Mat4 operator * (const Mat4& RHS);
 };
 
 struct Vertex
