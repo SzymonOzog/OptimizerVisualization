7093fad9cd85127555417ff9d1f776126445c403
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2023-07-05 08:40:50 +0200

    Add option to run in debug mode without compiler optimizations

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import os
+import sys
 from engine import *
 
 if __name__ == '__main__':
     if not os.path.exists("obj"):
         os.mkdir("obj")
-    os.system('g++ -static -shared -O3 -o obj/libbuffer.dll source/*.cpp')
+    debug_level = '' if sys.argv.count('DEBUG') > 0 else '-O3'
+    os.system(f'g++ -static -shared {debug_level} -o obj/libbuffer.dll -ggdb source/*.cpp')
     engine = Engine(1200,800)
     engine.start()
